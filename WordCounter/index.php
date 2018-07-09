36d2de51845d82811e6a9c4e2b67040403fc647a
setuproshad <39531895+setuproshad@users.noreply.github.com>
2018-07-09 08:47:35 +0600

    Add files via upload

---
+++ WordCounter/index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title> Character Frequency Counter </title>
+    </head>
+    
+    <body>
+        <br><br>
+        <hr>
+        <h1 align="center"> Character Frequency Counter </h1>
+        <hr>
+        
+        <h3 align="center">insert your string into the following text area to get the count per character</h3>
+        
+        <br>
+        
+        <form align="center" action ="show.php" method="POST">
+            
+            <textarea class="scrollabletextbox" rows="8" cols="90" name="sentString"></textarea>
+            
+            <br>
+            
+            <button type="submit" align="center">Count</button>
+            <button type="reset" align="center" value="reset">Clear</button>
+            
+        </form>
+        
+    </body>
+</html>
